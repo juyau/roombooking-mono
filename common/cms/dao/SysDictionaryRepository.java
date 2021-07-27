ac70d93f1f30e1f9056dddcb0542e35773d66d22
Sydneyfullstack <samuelju2017@gmail.com>
2021-07-27 18:34:40 +1000

    clean up extra files

--- common/cms/dao/SysDictionaryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.brothermiles.studyon.cms.dao;
-
-import com.brothermiles.studyon.model.cms.SysDictionary;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SysDictionaryRepository extends MongoRepository<SysDictionary, String> {
-
-    SysDictionary findByDName(String dName);
-}
--- common/exception/ExceptionCast.java
+++
@@ -1,9 +0,0 @@
-package com.brothermiles.studyon.common.exception;
-
-import com.brothermiles.studyon.common.model.response.ResultCode;
-
-public class ExceptionCast {
-    public static void cast(ResultCode resultCode){
-        throw new CustomException(resultCode);
-    }
-}
